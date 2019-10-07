d4d29aa222dd9e8b026a4bf89bf50332a1b08634
Anton <anton.anastasov@outlook.com>
2019-10-07 12:07:30 +0300

    Added memeViewHandler and memeViewModelFactory

--- Assignment/src/Server/SocketServer.java
+++ Assignment/src/Server/SocketServer.java
@@ -1,12 +1,26 @@
 package Server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketServer {
     public static void main(String[] args) {
         System.out.println("Server starting....");
+        int count=0;
         try{
             ServerSocket welcomeSocket=new ServerSocket(5600);
+            ConnectionPool connectionPool=new ConnectionPool();
+            while(true)
+            {
+                System.out.println("Waiting for client....");
+                Socket connSocket=welcomeSocket.accept();
+                ServerConnection serverConnection=new ServerConnection(connSocket,connectionPool);
+
+                new Thread(serverConnection,"Communication"+count).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
